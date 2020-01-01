69cf23199f52b1fda19b780def24b4bb16e482d4
Matthias Keiser <matthias@tristan-inc.com>
2020-01-01 20:45:57 +0100

    Update package

--- Package.swift
+++ Package.swift
@@ -1,5 +1,22 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
-	name: "BinarySearch"
+    name: "BinarySearch",
+    products: [
+        .library(
+            name: "BinarySearch",
+            targets: ["BinarySearch"]
+        )
+    ],
+    targets: [
+        // Our package contains two targets, one for our library
+        // code, and one for our tests:
+        .target(name: "BinarySearch"),
+        .testTarget(
+            name: "BinarySearchTests",
+            dependencies: ["BinarySearch"]
+        )
+    ]
 )
